5f4da509f8d786e0de287644b11cbcaa92f6326b
Stuart Beard <sebeard@me.com>
2022-05-24 17:11:30 +0100

    Update tenant variable -> Auth0 tenant for clarity

--- auth-token.js
+++ auth-token.js
@@ -1,16 +1,16 @@
 require('dotenv').config()
 const request = require('request-promise')
-const { TENANT, CLIENT_ID, CLIENT_SECRET, DEFAULT_SCOPE } = process.env
+const { AUTH0_TENANT, CLIENT_ID, CLIENT_SECRET, DEFAULT_SCOPE } = process.env
 
 async function accessToken() {
     return await request({
-        uri: `${TENANT}/oauth/token`,
+        uri: `${AUTH0_TENANT}/oauth/token`,
         json: true,
         method: 'POST',
         body: {
             client_id: `${CLIENT_ID}`,
             client_secret: `${CLIENT_SECRET}`,
-            audience: `${TENANT}/api/v2/`,
+            audience: `${AUTH0_TENANT}/api/v2/`,
             grant_type:"client_credentials"
           },
       })
--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const test = function () {
-  console.log("Hello World")
-}
+//External Libs
+require('dotenv').config()
+const request = require('request-promise')
+
+// Internal Libs
+const {accessToken} = require('./auth-token')
+
+const { AUTH0_TENANT } = process.env
+
+const test = async () => {
+    try {
+      const { token_type, access_token } = await accessToken();
+      const response = await request({
+        uri: `${AUTH0_TENANT}/api/v2/clients?fields=client_id,name,description&include_fields=true`,
+        json: true,
+        headers: {
+          authorization: [token_type, access_token].join(' '),
+        },
+      })
   
+      console.log(response)
+    } catch (error) {
+      console.log(`Error: ${error.message}`)
+    }
+  }
+    
 test()
