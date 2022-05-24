428330df1971b35d1ffc95d5b311d05d52890387
Stuart Beard <sebeard@me.com>
2022-05-24 17:26:40 +0100

    Add Express. Make API with endpoint to call into management API to list all client applications

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 //External Libs
 require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
 const request = require('request-promise')
 
 // Internal Libs
 const {accessToken} = require('./auth-token')
 
 const { AUTH0_TENANT } = process.env
+const app = express();
+app.use(bodyParser.json());
 
-const test = async () => {
+app.get('/list-clients', async (req, res) => {
+
     try {
-      const { token_type, access_token } = await accessToken();
-      const response = await request({
-        uri: `${AUTH0_TENANT}/api/v2/clients?fields=client_id,name,description&include_fields=true`,
-        json: true,
-        headers: {
-          authorization: [token_type, access_token].join(' '),
-        },
-      })
-  
-      console.log(response)
-    } catch (error) {
-      console.log(`Error: ${error.message}`)
-    }
-  }
-    
-test()
+        const { token_type, access_token } = await accessToken();
+        const response = await request({
+          uri: `${AUTH0_TENANT}/api/v2/clients2?fields=client_id,name,description&include_fields=true`,
+          json: true,
+          headers: {
+            authorization: [token_type, access_token].join(' '),
+          },
+        })
+        res.send(response);
+      } catch (error) {
+        console.log(`Error: ${error.message}`);
+        res.status(error.statusCode).send(`${error.message}`);
+      }
+});
+
+app.listen(8080, () => console.log(`Listening on port 8080`))
