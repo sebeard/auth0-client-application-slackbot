2b44280fcf59a481f0729eae4b02fb7c7da0fe99
Stuart Beard <sebeard@me.com>
2022-05-25 09:18:54 +0100

    Add new endpoint to update name and description of a client application

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //External Libs
 require('dotenv').config()
+const { App } = require('@slack/bolt');
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request-promise')
@@ -11,8 +12,9 @@ const { post } = require('request')
 const { AUTH0_TENANT } = process.env
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/list-clients', async (req, res) => {
+app.post('/list-clients', async (req, res) => {
 
     try {
         const { token_type, access_token } = await accessToken();
@@ -23,14 +25,15 @@ app.get('/list-clients', async (req, res) => {
             authorization: [token_type, access_token].join(' '),
           },
         })
-        res.send(response);
+
+        res.json(response)
       } catch (error) {
         console.log(`Error: ${error.message}`);
-        res.status(error.statusCode).send(`${error.message}`);
+        res.json({ text: `An error occurred during listing: ${error.message}` });
       }
 });
 
-app.post('/create-client/', async (req, res) => {
+app.post('/create-client', async (req, res) => {
 
     try {
         const { token_type, access_token } = await accessToken();
@@ -41,51 +44,76 @@ app.post('/create-client/', async (req, res) => {
           headers: {
             authorization: [token_type, access_token].join(' '),
           },
-          body : req.body,
+          body : {
+              name: req.body.text,
+              description: `${req.body.text}'s description`
+          }
         })
-        res.status(201).send(response);
+        res.json({ text: `Successully created ${req.body.text} with id ${response.client_id}` });
       } catch (error) {
         console.log(`Error: ${error.message}`);
-        res.status(error.statusCode).send(`${error.message}`);
+        res.json({ text: `An error occurred during creation: ${error.message}` });
       }
 });
 
-app.get('/get-client/:clientId', async (req, res) => {
-
+app.post('/get-client', async (req, res) => {
     try {
         const { token_type, access_token } = await accessToken();
         const response = await request({
-          uri: `${AUTH0_TENANT}/api/v2/clients/${req.params.clientId}?fields=client_id,name,description&include_fields=true`,
+          uri: `${AUTH0_TENANT}/api/v2/clients/${req.body.text}?fields=client_id,name,description&include_fields=true`,
           json: true,
           headers: {
             authorization: [token_type, access_token].join(' '),
           },
         })
-        res.send(response);
+        res.json({ text: `Client application (${response.client_id}) - name: ${response.name} description: ${response.description}` });
       } catch (error) {
         console.log(`Error: ${error.message}`);
-        res.status(error.statusCode).send(`${error.message}`);
+        res.json({ text: `An error occurred during retrieval: ${error.message}` });
       }
 });
 
-app.delete('/get-client/:clientId', async (req, res) => {
+app.post('/update-client', async (req, res) => {
+  // Inputs are [applicationId] [newName] [newDescription]
+  var inputs = req.body.text.toString().split(' ');
+  try {
+      const { token_type, access_token } = await accessToken();
+      const response = await request({
+        uri: `${AUTH0_TENANT}/api/v2/clients/${inputs[0]}`,
+        method: 'PATCH',
+        json: true,
+        headers: {
+          authorization: [token_type, access_token].join(' '),
+        },
+        body : {
+          name: `${inputs[1]}`,
+          description: `${inputs[2]}`
+        }
+      })
+      res.json({ text: `Client application (${response.client_id}) - name: ${response.name} description: ${response.description}` });
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+      res.json({ text: `An error occurred during retrieval: ${error.message}` });
+    }
+});
+
+app.post('/delete-client', async (req, res) => {
 
     try {
         const { token_type, access_token } = await accessToken();
         const response = await request({
-          uri: `${AUTH0_TENANT}/api/v2/clients/${req.params.clientId}?fields=client_id,name,description&include_fields=true`,
+          uri: `${AUTH0_TENANT}/api/v2/clients/${req.body.text}`,
           method: 'DELETE',
           json: true,
           headers: {
             authorization: [token_type, access_token].join(' '),
           },
         })
-        res.send(response);
+        res.send({ text: `Client application ${req.body.text} successfully deleted` });
       } catch (error) {
         console.log(`Error: ${error.message}`);
-        res.status(error.statusCode).send(`${error.message}`);
+        res.send({ text: `An error occurred during deletion: ${error.message}` });
       }
 });
-
 
 app.listen(8080, () => console.log(`Listening on port 8080`))
